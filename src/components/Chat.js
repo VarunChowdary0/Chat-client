5a2a57a5569cfe8ab43c9bed0503103a62855afd
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2023-11-03 20:34:34 +0530

    home

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -187,8 +187,21 @@ const Chat = () => {
     
   return (
     <div className="h-screen w-full bg-black/50 flex flex-col">
-        <div style={{ color: TextColor }} className={`h-[60px] bg-black/40 flex justify-between px-10 items-center text-2xl max-sm:text-sm max-sm:px-4`}>
-            <p><span className='text-yellow-500'>RoomID : </span> {roomID}</p>
+        <div style={{ color: TextColor }} className={`h-[60px] bg-black/40
+         flex justify-between px-10 items-center text-2xl max-sm:text-sm max-sm:px-4`}>
+            <a className='fixed' href='/o'>
+            <svg className=' fill-slate-50' xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512">
+                <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 
+                5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 
+                .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 
+                384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 
+                24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 
+                .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 
+                .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 
+                8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/>
+            </svg>
+            </a>
+            <p><span className='text-yellow-500 ml-[70px]'>RoomID : </span> {roomID}</p>
             <h1 style={{ color: TextColor }} className={`text-3xl `}>Chat</h1>
             <p>{username}</p>
         </div>
