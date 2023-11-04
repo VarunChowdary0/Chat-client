e4d583eb98328dfb0655fb2548233c3177bfbe5a
VarunChowdary0 <123965192+VarunChowdary0@users.noreply.github.com>
2023-11-04 23:29:27 +0530

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -438,7 +438,7 @@
                             <div style={{ color: TextColor }} className='h-[50px] bg-black/60 flex justify-center max-sm:text-sm text-center
                             fixed bottom-0 left-0 right-0 items-center gap-4 text-lg'>
                                                 Cannot reply in this room,
-                                    This is a private room only the owner send messages 
+                                    This is a private room only the owner can send messages 
                             </div>
                         </div>
                         :
