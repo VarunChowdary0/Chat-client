0ad93b333ceacfc4a9f4c2f3b6f4f98ce9698364
VarunChowdary0 <saivarunchowdarypoludasu4248@gmail.com>
2023-11-07 00:00:38 +0530

    hbvsd

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -775,16 +775,17 @@
                 })
             }
         </div>
-            <div onClick={GoBottom} className=' fixed h-[50px] w-[50px] bottom-5 
+            {!isCode && <div onClick={GoBottom} className=' fixed h-[50px] w-[50px] bottom-5
+             bg-[#5c5c5c] 
             right-4 flex items-center justify-center opacity-60 max-sm:hidden
-            rounded-full bg-[#606060] fill-white'>
+            rounded-full bg-[#6060 0] fill-white'>
             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512">
                 <path d="M169.4 502.6c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5
                  12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 402.7 
                  224 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 370.7L86.6 
                  329.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128z"/>
             </svg>
-            </div>
+            </div>}
         </div>
     )
     }
